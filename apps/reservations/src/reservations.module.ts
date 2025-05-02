435b3bcd26264b58d9b281e6c0a1109536ab9a7d
s0m-a <ogbennaanita@gmail.com>
2025-05-03 00:51:39 +0100

    added docker files

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,25 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule,LoggerModule } from '@app/common';
 import { ReservationDocument,ReservationSchema } from './models/reservation.schema';
 import { ReservationsRepository } from './reservations.repository';
-import {LoggerModule} from 'nestjs-pino'
 
 
 @Module({
   imports:[DatabaseModule, DatabaseModule.forFeature([
     {name:ReservationDocument.name, schema: ReservationSchema}]),
-  LoggerModule.forRoot({
-    pinoHttp:{
-      transport:{
-        target: 'pino-pretty',
-        options:{
-          singleLine: true
-        }
-      }
-    }
-  })
+  LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService,ReservationsRepository],

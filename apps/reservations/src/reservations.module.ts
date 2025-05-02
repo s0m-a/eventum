9a74a426a9b6199ae06f47354a9bdbf92724cc3f
s0m-a <ogbennaanita@gmail.com>
2025-05-02 09:59:43 +0100

    implemented validation and logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,10 +4,23 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationDocument,ReservationSchema } from './models/reservation.schema';
 import { ReservationsRepository } from './reservations.repository';
+import {LoggerModule} from 'nestjs-pino'
+
 
 @Module({
   imports:[DatabaseModule, DatabaseModule.forFeature([
-    {name:ReservationDocument.name, schema: ReservationSchema}])],
+    {name:ReservationDocument.name, schema: ReservationSchema}]),
+  LoggerModule.forRoot({
+    pinoHttp:{
+      transport:{
+        target: 'pino-pretty',
+        options:{
+          singleLine: true
+        }
+      }
+    }
+  })
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService,ReservationsRepository],
 })
